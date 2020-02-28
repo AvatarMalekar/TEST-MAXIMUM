3dc9a809f0c4305f89744af5c2f57e476948df10
Your Name <avatarmalekar4@gmail.com>
2020-02-28 17:33:25 +0530

    ADDED:function to find greatest String among three Strings

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -1,5 +1,5 @@
 public class MaximumNumber {
-    public int giveMaxNumber(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+    public int giveMaxValue(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
          Integer value = 0;
         if (firstNumber.compareTo(secondNumber) > 0) {
             if (firstNumber.compareTo(thirdNumber) > 0)
@@ -16,7 +16,7 @@ public class MaximumNumber {
         return value;
     }
 
-    public Float giveMaxNumber(Float firstNumber, Float secondNumber, Float thirdNumber) {
+    public Float giveMaxValue(Float firstNumber, Float secondNumber, Float thirdNumber) {
         Float value = 0f;
         if (firstNumber.compareTo(secondNumber) > 0) {
             if (firstNumber.compareTo(thirdNumber) > 0)
@@ -28,6 +28,23 @@ public class MaximumNumber {
                 value=secondNumber;
             } else {
                 value=thirdNumber;
+            }
+        }
+        return value;
+    }
+
+    public String giveMaxValue(String firstString, String secondString, String thirdString) {
+        String value = null;
+        if (firstString.compareTo(secondString) > 0) {
+            if (secondString.compareTo(thirdString) > 0)
+                value=firstString;
+            else
+                value=thirdString;
+        } else {
+            if (secondString.compareTo(thirdString) > 0) {
+                value=secondString;
+            } else {
+                value=thirdString;
             }
         }
         return value;
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -3,45 +3,63 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MaximumNumberTest {
-    MaximumNumber maxNumber;
+    MaximumNumber max;
     @Test
     public void givenNumbers_WhenProper_ShouldReturn_MaxNumber() {
-        int number = maxNumber.giveMaxNumber(8, 5, 2);
+        int number = max.giveMaxValue(8, 5, 2);
         Assert.assertEquals(8,number);
     }
 
     @Test
     public void givenSecondNumber_WhenMaximum_ShouldReturn_SecondNumber() {
-        int number = maxNumber.giveMaxNumber(5, 8, 2);
+        int number = max.giveMaxValue(5, 8, 2);
         Assert.assertEquals(8,number);
     }
 
     @Test
     public void givenThirdNumber_WhenMaximum_ShouldReturn_ThirdNumber() {
-        int number = maxNumber.giveMaxNumber(5, 2, 8);
+        int number = max.giveMaxValue(5, 2, 8);
         Assert.assertEquals(8,number);
     }
 
     @Test
     public void givenFirstFloatNumber_WhenMaximum_ShouldReturn_FirstNumber() {
-        float number = maxNumber.giveMaxNumber(7.55f, 2.15f, 5.21f);
+        float number = max.giveMaxValue(7.55f, 2.15f, 5.21f);
         Assert.assertEquals(7.21f,number,0.0);
     }
 
     @Test
     public void givenSecondFloatNumber_WhenMaximum_ShouldReturn_SecondNumber() {
-        float number = maxNumber.giveMaxNumber(5.55f, 7.15f, 2.21f);
+        float number = max.giveMaxValue(5.55f, 7.15f, 2.21f);
         Assert.assertEquals(7.15f,number,0.0);
     }
 
     @Test
     public void givenThirdFloatNumber_WhenMaximum_ShouldReturn_ThirdNumber() {
-        float number = maxNumber.giveMaxNumber(3.55f, 5.15f, 7.21f);
+        float number = max.giveMaxValue(3.55f, 5.15f, 7.21f);
         Assert.assertEquals(7.21f,number,0.0);
+    }
+
+    @Test
+    public void givenFirstString_WhenMaximum_ShouldReturn_FirstString() {
+        String value=max.giveMaxValue("rst","pqr","abc");
+        Assert.assertEquals("rst",value);
+    }
+
+    @Test
+    public void givenSecondString_WhenMaximum_ShouldReturn_SecondString() {
+        String value=max.giveMaxValue("abc","rst","pqr");
+        Assert.assertEquals("rst",value);
+    }
+
+    @Test
+    public void givenThirdString_WhenMaximum_ShouldReturn_ThirdString() {
+        String value=max.giveMaxValue("abc","pqr","rst");
+        Assert.assertEquals("rst",value);
     }
 
     @Before
     public void setUp() throws Exception {
-        maxNumber=new MaximumNumber();
+        max =new MaximumNumber();
     }
 }
